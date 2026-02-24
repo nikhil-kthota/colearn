0c6b66c07f5087c7c3e5bb8dd9cfa2f4cf1f7fc4
Nikhil Thota <nikhilkthota@gmail.com>
2026-02-24 21:21:24 +0530

    Restructure: Moved landing components to dedicated folder and removed unused assets/components

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
-import Home from '../components/Home';
-import HowItWorks from '../components/HowItWorks';
-import Features from '../components/Features';
-import Footer from '../components/Footer';
+import Navbar from '../components/LandingPage/Navbar';
+import Home from '../components/LandingPage/Home';
+import HowItWorks from '../components/LandingPage/HowItWorks';
+import Features from '../components/LandingPage/Features';
+import Footer from '../components/LandingPage/Footer';
 
 const LandingPage = ({ isDark, toggleTheme }) => {
     return (
